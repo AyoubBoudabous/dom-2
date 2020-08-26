55106cebe99866f59b70629753d8cc26818ba08f
AyoubBoudabous <69504108+AyoubBoudabous@users.noreply.github.com>
2020-08-26 11:08:21 +0100

    Add files via upload

---
+++ Dom/javascript.js
@@ -0,0 +1,12 @@
+let plus = document.getElementById("plus")
+let minus = document.getElementById("minus")
+let qte = document.querySelectorAll("btn btn-primary")
+let price = document.getElementById("btn btn-primary")
+
+
+
+
+for (let i = 0; i < plus.length; i++) {
+    plus[i].addEventListener("click", function() {
+        qte[i].innerHTML
+    })
